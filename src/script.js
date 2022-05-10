ed72144d0f2940e20f95ff67ff6e2b61c3ee5df2
sukumar_ganesan <apple@sukumars-MacBook-Air.local>
2022-05-10 22:43:27 +0530

    feat: added other obstacles and a score board

--- src/script.js
+++ src/script.js
@@ -13,6 +13,11 @@ const explosionLocation = {
     'loc': [0, 0, 0]
 }
 
+let game_score = {
+    hits: 0,
+    crashes: 0,
+}
+
 //lights
 const ambientLight = new THREE.AmbientLight(0xffffff, 1)
 const pointSourceLight = new THREE.PointLight(0xffffff, 1)
@@ -157,9 +162,15 @@ window.addEventListener('keydown', (event) => {
 });
 //collision sounds
 const laserSound = new Audio('/sounds/laser.mp3')
+const hitSound = new Audio('/sounds/hit-something.mp3')
 const playLaserSound = () => {
     laserSound.currentTime = 0
     laserSound.play()
+}
+
+const playHitSound = () => {
+    hitSound.currentTime = 0
+    hitSound.play()
 }
 
 // generate spheres
@@ -185,6 +196,27 @@ for (let i = 0; i < 10; i += 2) {
     scene.add(sp1, sp2)
 }
 
+const cube = new THREE.BoxGeometry(1, 1)
+
+const obstacle_pool = []
+for (let i = 0; i < 10; i += 2) {
+    let sp1 = new THREE.Mesh(cube, new THREE.MeshStandardMaterial())
+    sp1.material.metalness = 0.4
+    sp1.material.roughness = 0.7
+    sp1.position.set(Math.random() * 30, 0, Math.random() * (-100))
+    sp1.material.color = new THREE.Color('brown')
+
+    let sp2 = new THREE.Mesh(cube, new THREE.MeshStandardMaterial())
+    sp2.material.metalness = 0.4
+    sp2.material.roughness = 0.7
+    sp2.position.set(Math.random() * 30, 0, Math.random() * (-100))
+    sp2.material.color = new THREE.Color('brown')
+
+    obstacle_pool[i] = sp1
+    obstacle_pool[i + 1] = sp2
+    scene.add(sp1, sp2)
+}
+
 // particle explosion
 
 var movementSpeed = 80;
@@ -196,37 +228,40 @@ var colors = [0xFF0FFF, 0xCCFF00, 0xFF000F, 0x996600, 0xFFFFFF];
 var dirs = [];
 var parts = [];
 
-function ExplodeAnimation(x,y, color)
-{
+function ExplodeAnimation(x, y, color, score) {
+    score.hits++;
     var geometry = new THREE.Geometry();
 
-    for (let i = 0; i < totalObjects; i ++)
-    {
+    for (let i = 0; i < totalObjects; i++) {
         var vertex = new THREE.Vector3();
         vertex.x = x;
         vertex.y = y;
         vertex.z = 0;
 
-        geometry.vertices.push( vertex );
-        dirs.push({x:(Math.random() * movementSpeed)-(movementSpeed/2),y:(Math.random() * movementSpeed)-(movementSpeed/2),z:(Math.random() * movementSpeed)-(movementSpeed/2)});
+        geometry.vertices.push(vertex);
+        dirs.push({
+            x: (Math.random() * movementSpeed) - (movementSpeed / 2),
+            y: (Math.random() * movementSpeed) - (movementSpeed / 2),
+            z: (Math.random() * movementSpeed) - (movementSpeed / 2)
+        });
     }
-    var material = new THREE.ParticleBasicMaterial( { size: objectSize,  color: color});
-    var particles = new THREE.ParticleSystem( geometry, material );
+    var material = new THREE.ParticleBasicMaterial({size: objectSize, color: color});
+    var particles = new THREE.ParticleSystem(geometry, material);
 
     this.object = particles;
     this.status = true;
 
-    this.xDir = (Math.random() * movementSpeed)-(movementSpeed/2);
-    this.yDir = (Math.random() * movementSpeed)-(movementSpeed/2);
-    this.zDir = (Math.random() * movementSpeed)-(movementSpeed/2);
+    this.xDir = (Math.random() * movementSpeed) - (movementSpeed / 2);
+    this.yDir = (Math.random() * movementSpeed) - (movementSpeed / 2);
+    this.zDir = (Math.random() * movementSpeed) - (movementSpeed / 2);
 
-    scene.add( this.object  );
+    scene.add(this.object);
 
-    this.update = function(){
-        if (this.status == true){
+    this.update = function () {
+        if (this.status == true) {
             var pCount = totalObjects;
-            while(pCount--) {
-                var particle =  this.object.geometry.vertices[pCount]
+            while (pCount--) {
+                var particle = this.object.geometry.vertices[pCount]
                 particle.y += dirs[pCount].y;
                 particle.x += dirs[pCount].x;
                 particle.z += dirs[pCount].z;
@@ -236,10 +271,11 @@ function ExplodeAnimation(x,y, color)
     }
 
 }
-parts.push(new ExplodeAnimation(0, 0, 0xffffff));
+
+parts.push(new ExplodeAnimation(0, 0, 0xffffff, game_score));
 
 //
-const updateObjects = (object_pool, delta, x, spaceship, score) => {
+const updateObjects = (object_pool, obstacle_pool, delta, x, spaceship, score) => {
     for (let i = 0; i < 10; i += 2) {
         if (object_pool[i].position.z >= 0) {
             object_pool[i].position.x = Math.random() * (30 - x)
@@ -247,6 +283,15 @@ const updateObjects = (object_pool, delta, x, spaceship, score) => {
         } else {
             object_pool[i].position.z += delta
         }
+
+        if (obstacle_pool[i].position.z >= 0) {
+            obstacle_pool[i].position.x = Math.random() * (30 - x)
+            obstacle_pool[i].position.z = Math.random() * (-100) - 30
+        } else {
+            obstacle_pool[i].position.z += delta
+        }
+
+
         if (object_pool[i + 1].position.z >= 0) {
             object_pool[i + 1].position.x = Math.random() * (x - 30)
             object_pool[i + 1].position.z = Math.random() * (-100) - 30
@@ -254,24 +299,45 @@ const updateObjects = (object_pool, delta, x, spaceship, score) => {
             object_pool[i + 1].position.z += delta
         }
 
+        if (obstacle_pool[i + 1].position.z >= 0) {
+            obstacle_pool[i + 1].position.x = Math.random() * (x - 30)
+            obstacle_pool[i + 1].position.z = Math.random() * (-100) - 30
+        } else {
+            obstacle_pool[i + 1].position.z += delta
+        }
+
         //check for collisions
         if (spaceship.position.x <= object_pool[i].position.x + 1 && spaceship.position.x >= object_pool[i].position.x - 1) {
             //console.log("spaceship position: "+spaceship.position.x)
             if (spaceship.position.z <= object_pool[i].position.z + 1 && spaceship.position.z >= object_pool[i].position.z - 1) {
-                score++;
                 console.log(score);
                 playLaserSound()
-                parts.push(new ExplodeAnimation(spaceship.position.x, spaceship.position.y, object_pool[i].material.color));
-
+                parts.push(new ExplodeAnimation(spaceship.position.x, spaceship.position.y, object_pool[i].material.color, game_score));
             }
         }
         if (spaceship.position.x <= object_pool[i + 1].position.x + 1 && spaceship.position.x >= object_pool[i + 1].position.x - 1) {
             //console.log("spaceship position: "+spaceship.position.x)
             if (spaceship.position.z <= object_pool[i + 1].position.z + 1 && spaceship.position.z >= object_pool[i + 1].position.z - 1) {
-                score++;
                 console.log(score);
                 playLaserSound()
-                parts.push(new ExplodeAnimation(spaceship.position.x, spaceship.position.y, object_pool[i+1].material.color));
+                parts.push(new ExplodeAnimation(spaceship.position.x, spaceship.position.y, object_pool[i + 1].material.color, game_score));
+            }
+        }
+
+        if (spaceship.position.x <= obstacle_pool[i].position.x + 1 && spaceship.position.x >= obstacle_pool[i].position.x - 1) {
+            //console.log("spaceship position: "+spaceship.position.x)
+            if (spaceship.position.z <= obstacle_pool[i].position.z + 1 && spaceship.position.z >= obstacle_pool[i].position.z - 1) {
+                console.log(score);
+                playHitSound();
+                game_score.crashes++;
+            }
+        }
+        if (spaceship.position.x <= obstacle_pool[i + 1].position.x + 1 && spaceship.position.x >= obstacle_pool[i + 1].position.x - 1) {
+            //console.log("spaceship position: "+spaceship.position.x)
+            if (spaceship.position.z <= obstacle_pool[i + 1].position.z + 1 && spaceship.position.z >= obstacle_pool[i + 1].position.z - 1) {
+                console.log(score);
+                playHitSound()
+                game_score.crashes++;
             }
         }
     }
@@ -315,6 +381,9 @@ gui.hide()
 const clock = new THREE.Clock()
 let oldElapsedTime = 0
 const animate = () => {
+    document.getElementById('hit').innerHTML = "hits: " + Math.floor(game_score.hits / 3);
+    document.getElementById('crashes').innerHTML = "crashes: " + Math.floor(game_score.crashes / 3);
+
     const currentElapsedTime = clock.getElapsedTime()
     const delta = currentElapsedTime - oldElapsedTime
     oldElapsedTime = currentElapsedTime
@@ -322,7 +391,7 @@ const animate = () => {
     world.step(1 / 60, delta, 3)
     // move grid backwards along with all the objects
     grid.material.uniforms.time.value += delta * 20;
-    updateObjects(object_pool, delta * 20, spaceship.position.x, spaceship)
+    updateObjects(object_pool, obstacle_pool, delta * 40, spaceship.position.x, spaceship, game_score)
 
 
     // drag
@@ -345,7 +414,7 @@ const animate = () => {
     pointSourceLight.position.copy(spaceship.position).add(new THREE.Vector3(0, 2, 4))
 
     let pCount = parts.length;
-    while(pCount--) {
+    while (pCount--) {
         parts[pCount].update();
     }
 
